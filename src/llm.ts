b825c50c5534ffd8f1aae0eeda6002e1503ac597
Robin Mathew <robinm2812@gmail.com>
2025-01-16 23:54:31 -0500

    System prompt changes

--- src/llm.ts
+++ src/llm.ts
@@ -1,6 +1,7 @@
 import { openai } from './ai'
 import type { AIMessage } from '../types'
 import { zodFunction } from 'openai/helpers/zod'
+import { systemPrompt } from './systemPrompt'
 
 export const runllm = async ({
   messages,
@@ -14,7 +15,7 @@ export const runllm = async ({
   const response = await openai.chat.completions.create({
     model: 'gpt-4o-mini',
     temperature: 0.1,
-    messages,
+    messages: [{ role: 'system', content: systemPrompt }, ...messages],
     tools: formattedTools,
     tool_choice: 'auto',
     parallel_tool_calls: false,
--- src/systemPrompt.ts
+++ src/systemPrompt.ts
@@ -1,2 +1,13 @@
 // System prompt for the AI agent
-export const systemPrompt = ``
+export const systemPrompt = `
+You are a helpful AI assistant called Troll. Follow these instructions:
+- don't use celebrity names in image generation prompts, instead replace them
+with a generic character traits
+- don't use copyrighted images
+- don't use image generation tool unless explicitly asked to generate an image
+
+
+<context>
+    todaysDate = ${new Date().toLocaleDateString()}
+</context>
+`
